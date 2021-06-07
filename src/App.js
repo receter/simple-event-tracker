dd0ecc2c25d97937b92e8ec26b5447a114cb1c31
Andreas Riedmueller <andreas@simplease.at>
2021-06-07 09:52:53 +0200

    improved styling, detail view, add past events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import { useReducer, useState } from 'react';
-import logo from './logo.svg';
+import dayjs from 'dayjs';
+import { v4 as uuidv4 } from 'uuid';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import * as styles from './styles.module.css';
+
+dayjs.extend(relativeTime);
+
+function sortTrackersByLatestEvent(a, b) {
+  const latestDateA = dayjs(a.events[a.events.length - 1]);
+  const latestDateB = dayjs(b.events[b.events.length - 1]);
+  if (latestDateA.isAfter(latestDateB)) {
+    return -1;
+  } else {
+    return 1
+  }
+}
 
 const initialState = {
   trackers: []
@@ -9,7 +23,13 @@ const initialState = {
 function initState(initialState) {
   const trackersJson = localStorage.getItem('trackers');
   if (trackersJson) {
-    const trackers = JSON.parse(trackersJson);
+    const trackers = JSON.parse(trackersJson).map(tracker => {
+      // For backwards compatibility
+      if (!tracker.id) {
+        tracker.id = uuidv4();
+      }
+      return tracker;
+    });
     return { ...initialState, trackers }
   }
   return initialState;
@@ -18,19 +38,27 @@ function initState(initialState) {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_TRACKER': {
-      const updatedTrackers = [...state.trackers, { name: action.name, events: [] }];
+      const updatedTrackers = [...state.trackers, { id: uuidv4(), name: action.name, events: [] }];
       localStorage.setItem('trackers', JSON.stringify(updatedTrackers));
       return { ...state, trackers: updatedTrackers }
     }
     case 'TRACK_EVENT': {
-      const { id } = action;
+      const { id, time } = action;
       const updatedTrackers = state.trackers.map((tracker, index) => {
-        if (index === id) {
-          return { ...tracker, events: [...tracker.events, Date.now()] }
+        if (tracker.id === id) {
+          return { ...tracker, events: [...tracker.events, time] }
         } else {
           return tracker;
         }
       });
+      localStorage.setItem('trackers', JSON.stringify(updatedTrackers));
+      return { ...state, trackers: updatedTrackers };
+    }
+    case 'DELETE_TRACKER': {
+      const { id } = action;
+      const updatedTrackers = [...state.trackers];
+      const indexOfTracker = updatedTrackers.findIndex(tracker => tracker.id === id);
+      updatedTrackers.splice(indexOfTracker, 1);
       localStorage.setItem('trackers', JSON.stringify(updatedTrackers));
       return { ...state, trackers: updatedTrackers };
     }
@@ -45,6 +73,8 @@ function App() {
   const [newTrackerActive, setNewTrackerActive] = useState(false);
   const [trackEventActive, setTrackEventActive] = useState(false);
   const [trackEventTrackerId, setTrackEventTrackerId] = useState(null);
+  const [trackEventTime, setTrackEventTime] = useState(null);
+  const [trackerDetailView, setTrackerDetailView] = useState(null);
 
   function handleClickNewTracker() {
     setCreateTrackerName('');
@@ -57,12 +87,13 @@ function App() {
   }
 
   function handleClickTrackEvent(trackerId) {
+    setTrackEventTime(Date.now());
     setTrackEventTrackerId(trackerId);
     setTrackEventActive(true);
   }
 
   function handleClickSaveEvent() {
-    dispatch({ type: 'TRACK_EVENT', id: trackEventTrackerId });
+    dispatch({ type: 'TRACK_EVENT', id: trackEventTrackerId, time: trackEventTime });
     setTrackEventActive(false);
   }
 
@@ -74,6 +105,60 @@ function App() {
     setNewTrackerActive(false);
   }
 
+  function handleClickTracker(trackerId) {
+    setTrackerDetailView(state.trackers.find(tracker => tracker.id === trackerId));
+  }
+
+  function handleClickCloseDetailView() {
+    setTrackerDetailView(null);
+  }
+
+  function handleClickDeleteTracker(trackerId) {
+    if (window && window.confirm('Are you sure you want to delete this tracker?')) {
+      setTrackerDetailView(null);
+      dispatch({ type: 'DELETE_TRACKER', id: trackerId });
+    }
+  }
+
+  const sortedTrackers = state.trackers.sort(sortTrackersByLatestEvent);
+
+  if (trackerDetailView) {
+    const tracker = trackerDetailView;
+    const firstEvent = tracker.events[0];
+    const lastEvent = tracker.events[tracker.events.length - 1];
+    const dateNow = dayjs();
+    const dateCreated = dayjs(firstEvent);
+    const daysSinceCreated = Math.max(14, Math.ceil(dateNow.diff(dateCreated, 'days', true)));
+    const calendarDays = [];
+    for (let i = 0; i < daysSinceCreated; i++) {
+      calendarDays.push(dateCreated.subtract(i, 'day'));
+    }
+    return <div className={styles.app}>
+      <div className={styles.detailView}>
+        <button className={styles.detailViewClose} onClick={handleClickCloseDetailView} type={'button'}>Close</button>
+        <div className={styles.detailViewCount}>{tracker.events.length}</div>
+        <h1 className={styles.detailViewTitle}>{tracker.name}</h1>
+        <div className={styles.detailViewLast}>{dayjs(lastEvent).fromNow()}</div>
+        <div className={styles.detailViewCalendar}>
+          {calendarDays.map((day, index) => <div
+            key={index}
+            className={day.format('dd') === 'Su' ? styles.detailViewDay + ' ' + styles.detailViewSunday : styles.detailViewDay}
+          >
+            <div>{tracker.events.filter(event => dayjs(event).isSame(day, 'day')).length}</div>
+            <div>{day.format('dd D.M')}</div>
+          </div>)}
+        </div>
+        <button
+          className={styles.detailViewDelete}
+          onClick={() => handleClickDeleteTracker(tracker.id)}
+          type={'button'}
+        >
+          Delete Tracker
+        </button>
+      </div>
+    </div>
+  }
+
   return (
     <div className={styles.app}>
       <header className={styles.header}>
@@ -83,18 +168,26 @@ function App() {
       <div
         className={styles.trackers}
       >
-        {state.trackers.map((tracker, index) => <div
-          key={index}
+        {sortedTrackers.map(tracker => <div
+          key={tracker.id}
           className={styles.tracker}
         >
-          <div className={styles.trackerCount}>{tracker.events.length}</div>
-          <div className={styles.trackerName}>{tracker.name}</div>
+          <div className={styles.trackerDetails} onClick={() => handleClickTracker(tracker.id)}>
+            <div className={styles.trackerCount}>
+              <div>{tracker.events.length}</div>
+            </div>
+            <div className={styles.trackerName}>
+              <div>
+                <div>{tracker.name}</div>
+                <div className={styles.trackerInfo}>{dayjs(tracker.events[tracker.events.length - 1]).fromNow()}</div>
+              </div>
+            </div>
+          </div>
           <button
-            className={styles.trackerTrack}
             type={'button'}
-            onClick={() => handleClickTrackEvent(index)}
-          >Track
-          </button>
+            className={styles.trackerTrack}
+            onClick={() => handleClickTrackEvent(tracker.id)}
+          />
         </div>)}
       </div>
       {newTrackerActive && <div className={styles.createTracker}>
@@ -104,7 +197,36 @@ function App() {
       </div>}
       {trackEventActive && <div className={styles.trackEvent}>
         <div className={styles.trackEventTitle}>
-          1 x {state.trackers[trackEventTrackerId].name}
+          1 &times; {state.trackers.find(tracker => tracker.id === trackEventTrackerId).name}
+        </div>
+        <div className={styles.trackEventTime}>
+          {dayjs(trackEventTime).format('dd D.M HH:mm')}
+          <div className={styles.timeManipulationButtons}>
+            <button
+              className={styles.subtractDayButton}
+              type={'button'}
+              onClick={() => setTrackEventTime(dayjs(trackEventTime).subtract(1, 'day').valueOf())}
+            >- Day
+            </button>
+            <button
+              className={styles.addDayButton}
+              type={'button'}
+              onClick={() => setTrackEventTime(dayjs(trackEventTime).add(1, 'day').valueOf())}
+            >+ Day
+            </button>
+            <button
+              className={styles.subtractHourButton}
+              type={'button'}
+              onClick={() => setTrackEventTime(dayjs(trackEventTime).subtract(1, 'hour').valueOf())}
+            >- Hour
+            </button>
+            <button
+              className={styles.addHourButton}
+              type={'button'}
+              onClick={() => setTrackEventTime(dayjs(trackEventTime).add(1, 'hour').valueOf())}
+            >+ Hour
+            </button>
+          </div>
         </div>
         <button className={styles.trackEventSave} onClick={handleClickSaveEvent}>Save</button>
         <button className={styles.trackEventDiscard} onClick={handleClickDiscardEvent}>Discard</button>
